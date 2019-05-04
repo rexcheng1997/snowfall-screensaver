ffa03cddb75970445bce1dc19ea9f361ca17f0e9
rexcheng <rexcheng@umich.edu>
2019-05-03 21:26:31 -0400

    add c# source

---
+++ cs/screensaver/screensaver/MyScreen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace screensaver
+{
+    public partial class MyScreen : Form
+    {
+        private readonly int id;
+
+        public MyScreen(int _id)
+        {
+            InitializeComponent();
+            id = _id;
+            KeyPress += new KeyPressEventHandler(MyScreen_OnKeyPress);
+        }
+
+        private void MyScreen_Load(object sender, EventArgs e)
+        {
+            Bounds = Screen.AllScreens[id].Bounds;
+            Cursor.Hide();
+            TopMost = true;
+        }
+
+        private void MyScreen_OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ cs/screensaver/screensaver/Screensaver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace screensaver
+{
+    static class Screensaver
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (args.Length > 0)
+            {
+                string tag = args[0].ToLower().Trim().Substring(0, 2);
+                switch (tag)
+                {
+                    // show
+                    case "/s":
+                        break;
+
+                    // preview
+                    case "/p":
+                        break;
+
+                    // configure
+                    case "/c":
+                        MessageBox.Show("Sorry, configuration options are not available at this point.", 
+                            "Snowfall Screensaver", 
+                            MessageBoxButtons.OK, 
+                            MessageBoxIcon.Information);
+                        break;
+
+                    default:
+                        MessageBox.Show("Command line argument error: \"" + tag + "\" is not found", 
+                            "Snowfall Screensaver", 
+                            MessageBoxButtons.OK, 
+                            MessageBoxIcon.Exclamation);
+                        break;
+                }
+            }
+            else
+            {
+                ShowScreens();
+            }
+        }
+
+        static private void ShowScreens()
+        {
+            for (int i = Screen.AllScreens.GetLowerBound(0); i <= Screen.AllScreens.GetUpperBound(0); i++)
+            {
+                Application.Run(new MyScreen(i));
+            }
+        }
+    }
+}
